5031ea73052323cb6deebb96f5c1df65c89cd86d
juliecst <julia_stamm@yahoo.de>
2023-03-03 13:18:10 +0000

    lol

--- sth.js
+++ sth.js
@@ -5,8 +5,9 @@
         'http://stackoverflow.com',
         'https://github.com/juliecst/annaRoom/settings/pages',
         'https://www.window-swap.com/Window',
-        'https://www.bbc.co.uk/news/uk-politics-64815875'
-    
+        'https://www.bbc.co.uk/news/uk-politics-64815875',
+        'https://juliecst.github.io/artistPage/',
+        'https://juliecst.github.io/annaRoom/'
       ];
     var site = sites[Math.floor(Math.random() * sites.length)];
     window.open(site, '_blank').focus();
